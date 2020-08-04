fb5c87d965bdaa0f0ce8b4116ce377ad89ce5cc0
Bruno Mayerle Leite <brleite@gmail.com>
2020-08-04 10:52:27 -0300

    Commit inicial

--- MinhasTarefasAPI/Startup.cs
+++ MinhasTarefasAPI/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MinhasTarefasAPI.Database;
 
 namespace MinhasTarefasAPI
 {
@@ -26,6 +28,10 @@ namespace MinhasTarefasAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MinhasTarefasContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("MinhasTarefasContext"), builder =>
+                    builder.MigrationsAssembly("MinhasTarefasAPI")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 

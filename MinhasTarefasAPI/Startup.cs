3f93bffeb1ba52f3819fc934625cf5044e71548a
Bruno Mayerle Leite <brleite@gmail.com>
2020-08-04 10:48:53 -0300

    Teste

--- MinhasTarefasAPI/Startup.cs
+++ MinhasTarefasAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace MinhasTarefasAPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
